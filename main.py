1196a873e19a3483d08db8980023161a28b13bcf
Adam Ham <78129424+hidden0719@users.noreply.github.com>
2023-06-28 14:07:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'''
+Title: Evidential Meta-Learning for Molecular Property Prediction
+Authors:
+- KP Ham, Lee Sael (sael@ajou.ac.kr) Ajou University
+This software may be used only for research evaluation purposes.
+For other purposes (e.g., commercial), please contact the authors.
+'''
 import os
 print('pid:', os.getpid())
 import numpy as np
